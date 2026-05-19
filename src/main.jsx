26e2f572c775dcc753b445de5dc801d5ad133aa4
appopsinsight-sudo <appopsinsight@gmail.com>
2026-05-19 07:44:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import GymLink from './App.jsx'
 import './index.css'
 
+window.__GMAPS_KEY = import.meta.env?.VITE_GOOGLE_MAPS_API_KEY || "";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GymLink />
